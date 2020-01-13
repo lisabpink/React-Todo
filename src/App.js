1412017006d7030779abef6d9c97e87444dd9f35
LISA_COOK <lisabpink@gmail.com>
2020-01-13 12:04:16 -0500

    Getting started, small styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import "./components/TodoComponents/Todo.css";
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+        super();
+        this.state = {
+            name:"Lisa"
+                };
+      }
   render() {
     return (
       <div>
-        <h2>Welcome to Lisa'sTodo App!</h2>
+    <h1>Hello! Welcome to  {this.state.name}'s To Do App!!!</h1>
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,20 @@
+// import React from "react";
+
+// class TodoForm extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             name:"Lisa"
+//                 };
+//       }
+
+//       render(){
+//         console.log("rendering form");
+//           return(
+//             <div className="App">
+//         <h1>Hello!!!{this.state.name}</h1>
+//         <h2>Magic here!</h2>
+//       </div>
+//           );
+//       }
+//       export default TodoForm;
